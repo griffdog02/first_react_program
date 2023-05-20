41d6d0df66d6969ba2a4c9ce98bff63220a1eade
Jordan Griffith <jlg02@cs.washington.edu>
2023-05-19 18:06:40 -0700

    adding files

---
+++ styles.js
@@ -0,0 +1,20 @@
+const styles = {
+    container: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%'
+    },
+    text: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+
+    iconContainer: {
+        width: 150,
+        height: 150,
+    },
+};
+
+export default styles;
